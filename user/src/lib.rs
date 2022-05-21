dcba8223b0229ab5895d46e534aed8a23efea9ce
qi-xmu <360141773@qq.com>
2022-05-21 17:10:47 +0800

    .

--- user/src/lib.rs
+++ user/src/lib.rs
@@ -39,6 +39,25 @@ fn main() -> i32 {
     panic!("Cannot find main!");
 }
 
+#[macro_use]
+extern crate bitflags;
+bitflags! {
+    pub struct OpenFlags:u32{
+        const RDONLY = 0;
+        const WRONLY = 1<<0;
+        const RDWR = 1<<1;
+        const CREATE = 1<<9;
+        const TRUBC  = 1<<10;
+    }
+}
+pub fn open(path: &str, flags: OpenFlags) -> isize {
+    sys_open(path, flags.bits)
+}
+
+pub fn close(fd: usize) -> isize {
+    sys_close(fd)
+}
+
 pub fn read(fd: usize, buf: &mut [u8]) -> isize {
     sys_read(fd, buf)
 }
